ba930f45980128fe1e192dbdf27e5fc5f4bf33bd
Lucy Wall <lhwall@gmail.com>
2018-11-07 19:25:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  return Math.abs(pickup - 42)
+}
+
+function blocksToFeet(blocks){
+  return blocks * 264
+}
+
+function distanceFromHqInFeet(pickup){
+  return blocksToFeet(distanceFromHqInBlocks(pickup))
+}
+
+function distanceTravelledInFeet(start, end){
+  return blocksToFeet(Math.abs(start - end))
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination)
+  let price;
+  if (distance <= 400) {
+    price = 0 }
+  else if  (distance <= 2000){
+    price = (distance -400) * .02
+  }
+  else if (distance <= 2500){
+    price = 25
+  }
+    else {
+      price = "cannot travel that far"
+    }
+    return price
+  }
