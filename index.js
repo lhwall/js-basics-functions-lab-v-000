225adae6f48296aaf86d1bf5ced8833a30f45372
Lucy Wall <lhwall@gmail.com>
2018-11-07 19:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function distanceTravelledInFeet(start, end){
 
 function calculatesFarePrice(start, destination){
   let distance = distanceTravelledInFeet(start, destination)
-  
+  let price = 0 
   switch (distance) {
     case distance <= 400:
       price = 0
