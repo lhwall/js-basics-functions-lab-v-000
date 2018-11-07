b3206c098068fb70a92ff1a448544a79acae59cc
Lucy Wall <lhwall@gmail.com>
2018-11-07 18:49:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function distanceFromHqInBlocks(pickup) {
   return Math.abs(pickup - 42)
 }
+
+function blocksToFeet(blocks){
+  return blocks * 264
+}
+
+function distanceFromHqInFeet(pickup){
+  blocksToFeet(distanceFromHqInBlocks(pickup))
+}
